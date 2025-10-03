de575eb5eab129e4f138ae35337393009f78eb3c
ktechhydle <kellerhydle@gmail.com>
2025-10-02 19:42:35 -0600

    Backend Updates
    -add data retrieving

--- src/script.js
+++ src/script.js
@@ -12,6 +12,18 @@ function closeErrorDialog() {
   errorDialog.close();
 }
 
+async function createOutput() {
+  const formData = new FormData(form);
+  const start = formData.get("start");
+  const end = formData.get("end");
+  const repeat = formData.get("repeat");
+  const prefix = formData.get("prefix");
+  const vertical = document.getElementById("count-numbers-vertically").checked;
+  const numbered = document.getElementById("number-spots").checked;
+
+  console.log(startRange);
+}
+
 function downloadOutput() {
   const formData = new FormData(form);
   const filename = formData.get("filename");
@@ -28,3 +40,6 @@ function downloadOutput() {
 window.closeErrorDialog = closeErrorDialog;
 window.showErrorDialog = showErrorDialog;
 window.downloadOutput = downloadOutput;
+
+// change the output preview everytime a form value is modified
+form.addEventListener("input", createOutput);
